0cca735847c405a37ba0f423f8ffd73934ad36a2
guneycansanli <guney@zee.ro>
2023-05-16 14:03:08 -0400

    TS let and const(fuction)

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,20 @@
-var character = "luigi";
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var character = 'test';
+var age = 30;
+var isBlackBelt = false;
+//Let can not chance to another type
+//character = 2;
+character = 'luigi';
+//age can not be string
+//age = '2';
+age = 40;
+//isBlackBelt must be boolen
+//isBlackBelt = 'yes';
+isBlackBelt = true;
+//Ts can access all libraryies from JS Math is JS library and TS can use it
+//Diameter must be number
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+//It will return NaN (Not a Number)
+//console.log(circ("hello"));
+console.log(circ(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,28 @@
-const character = "luigi";
+let character = 'test';
+let age = 30;
+let isBlackBelt = false;
 
-console.log(character);
+//Let can not chance to another type
+//character = 2;
 
-const inputs = document.querySelectorAll('input');
+character = 'luigi';
 
-console.log(inputs);
+//age can not be string
+//age = '2';
 
-inputs.forEach(input => {
-    console.log(input);
-});
+age = 40;
+
+//isBlackBelt must be boolen
+//isBlackBelt = 'yes';
+isBlackBelt = true;
+
+
+//Ts can access all libraryies from JS Math is JS library and TS can use it
+//Diameter must be number
+const circ = (diameter: number) => {
+    return diameter * Math.PI
+};
+
+//It will return NaN (Not a Number)
+//console.log(circ("hello"));
+console.log(circ(7.5));
